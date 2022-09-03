fdec858074cd73046775d56bfe620a302a1bc533
ChrisOmeh <45576847+ChrisOmeh@users.noreply.github.com>
2022-09-03 14:13:53 +0100

    Add files via upload
    DaaS files

---
+++ DaaS_Poweershell_assignment.ps1
@@ -0,0 +1,23 @@
+﻿
+#QUESTION 1
+Get-Service | Where-Object {$_.Status -eq "Stopped"}
+
+#QUESTION 2
+Get-Service   "A", "X", "R", "W"
+
+ #QUESTION 3
+ Get-Service | Where-Object {$_.StartType -eq "Automatic"} > service_automatic_start.json
+ 
+#QUESTION 4
+Get-Service | select -Property name, status > export_serviceName_question4.txt
+
+#QUESTION 5
+Get-Service | select -Property name, status > export_serviceName_question5.html
+
+#FOR PROCESS
+
+#QUESTION 1
+Get-Process   "T*", "N*", "S*"
+
+#QUESTION 2
+Get-Process  | select -ProcessName PM(K) Where ProcessName = "svchost"AND PM(K) > 100
---
+++ Power shell script.ps1
@@ -0,0 +1,8 @@
+﻿Get-Verb
+Get-command
+Get-Member
+Get-help
+
+
+
+Get-Command -Name Copy
